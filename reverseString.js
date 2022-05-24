c108ae7816616a546e63c0b7e8439653986ea22a
Shiloh Rabena <shilohrabena@gmail.com>
2022-05-24 12:25:50 -0700

    Added a second, more efficient, function that reverses a string without .reverse()

--- reverseString.js
+++ reverseString.js
@@ -4,3 +4,8 @@ function reverseString(str) {
     for(let i = 0; i < l; i+=2) a.unshift(a[i])
     return a.slice(0,a.length/2).join('')
 }
+
+function reverseStringV2(str) {
+    for(let i = str.length-1; i >=0 ; i--) str += str.charAt(i)
+    return str.slice(-str.length/2)
+}
